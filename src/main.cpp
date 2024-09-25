eb0fec1857a22363ee2c0b21305a4270a5be832a
Alejandro Álvarez Ayllón <alejandro.alvarez@sonarsource.com>
2024-09-25 17:12:15 +0200

    Enable modules analysis

--- src/main.cpp
+++ src/main.cpp
@@ -1,26 +1,22 @@
 #include <iostream>
+#include <variant>
+
+import args;
 
 using namespace std;
 
-int main(int argc, char* argv[]) {
-  int num = argc - 1;
-
-  if (num == 0) {
-    cout << "No arguments provided\n";
-  } else if (num == 0) { // intentional mistake
-    cout << "1 argument provided\n";
-  } else if (num == 2) {
-    cout << "2 arguments provided\n";
-  } else {
-    cout << num << " arguments provided\n";
-  }
-  if (argv != 0) {
-    cout << "argv not null\n";; // intentional extra-semicolon
-  }
-  if (argv == nullptr) {
-    return **argv; // intentional nullptr dereference
+int main(int argc, char *argv[]) {
+  auto get_proc_name = args::process_args(argc, argv);
+  if (std::holds_alternative<args::Error>(get_proc_name)) {
+    switch (std::get<args::Error>(get_proc_name)) {
+    case args::Error::TooLong:
+      std::cout << "Proc name too long\n";
+      return 1;
+    }
+    return 0;
   }
 
+  auto &&value = std::get<std::string_view>(get_proc_name);
+  std::cout << value << '\n';
   return 0;
 }
-
